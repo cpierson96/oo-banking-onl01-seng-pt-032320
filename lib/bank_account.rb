97cd11498c74c7d2ddc7d2ff9094e50f5a5eecd3
Claudia Pierson <c172@umbc.edu>
2020-05-02 06:38:10 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,19 @@
 class BankAccount
-
-end
+  attr_accessor :balance, :status
+  attr_reader :name 
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = "open" 
+  end 
+  def deposit(guap) 
+    self.balance += guap 
+  end 
+  def display_balance 
+   "Your balance is $#{self.balance}."
+  end 
+  
+  def valid? 
+   status==open? && balance > 0 =! valid
+  end 
+  end
